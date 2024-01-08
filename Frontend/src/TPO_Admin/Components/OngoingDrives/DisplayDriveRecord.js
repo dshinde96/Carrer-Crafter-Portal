5c36484594c8387046524785abacf1d91f2a257c
dshinde <dnyaneshwarshinde33368@gmail.com>
2024-01-08 10:19:05 +0530

    Second commit- Updated frontend at TPO Admin side

--- Frontend/src/TPO_Admin/Components/OngoingDrives/DisplayDriveRecord.js
+++ Frontend/src/TPO_Admin/Components/OngoingDrives/DisplayDriveRecord.js
@@ -10,30 +10,32 @@ const DriveRecord_cnt = (props) => {
     const { curDrive, DriveStu } = useContext(TpoAdminContext);
     const params = useParams();
     const handleDownload = () => {
-    
+
         // Convert JSON to Excel
-        const students=[];
-        DriveStu.map((stu)=>{
-            students.push({name:stu.profile.name,
-                email:stu.profile.email,
-                "Moblie No":`${stu.profile.mob_no}`,
-                "Date of birth":`${new Date(stu.profile.dob).getDate()}/${new Date(stu.profile.dob).getMonth()}/${new Date(stu.profile.dob).getFullYear()}`,
-                Department:stu.profile.dept,
-                year:stu.profile.year,
-                "10th Percentage":stu.Percentage10th,
-                "12th/Diploma Percentage":stu.Percentage10th,
-                "Btech CGPA":stu.Percentage10th})
+        const students = [];
+        DriveStu.map((stu) => {
+            students.push({
+                name: stu.profile.name,
+                email: stu.profile.email,
+                "Moblie No": `${stu.profile.mob_no}`,
+                "Date of birth": `${new Date(stu.profile.dob).getDate()}/${new Date(stu.profile.dob).getMonth()}/${new Date(stu.profile.dob).getFullYear()}`,
+                Department: stu.profile.dept,
+                year: stu.profile.year,
+                "10th Percentage": stu.Percentage10th,
+                "12th/Diploma Percentage": stu.Percentage10th,
+                "Btech CGPA": stu.Percentage10th
+            })
         })
         const ws = XLSX.utils.json_to_sheet(students);
-    
+
         // Create a workbook and add the worksheet
         const wb = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    
+
         // Save the workbook as an Excel file
         const fileName = `${curDrive.CompanyName} Eligible Students.xlsx`;
         XLSX.writeFile(wb, fileName);
-      };
+    };
     return (
         <>
             <div className="container">
@@ -44,8 +46,11 @@ const DriveRecord_cnt = (props) => {
                 <p>ExpectedOpening: <spam>{curDrive.ExpectedOpening}</spam></p>
                 <p>EligibilityCriteria: <spam>{curDrive.EligibilityCriteria}</spam></p>
             </div>
-            <div className="container">
-                <h4>Applications</h4>
+            <div className="container mt-4">
+                {params.category == "InterestedStu" && <h4>Applications</h4>}
+                {params.category == "EligibleStu" && <h4>Shortlisted Students</h4>}
+                {params.category == "SelectedStu" && <h4>Selected Students</h4>}
+                <h4></h4>
                 <table>
                     <tr>
                         <th>Sr no</th>
@@ -57,12 +62,12 @@ const DriveRecord_cnt = (props) => {
                         <th>Placed</th>
                         <th>Action</th>
                     </tr>
-                    {DriveStu && DriveStu.map((stu, index) => <Row stu={stu} index={index + 1} driveId={curDrive._id} cat={params.category}/>)}
+                    {DriveStu && DriveStu.map((stu, index) => <Row stu={stu} index={index + 1} driveId={curDrive._id} cat={params.category} />)}
 
                 </table>
 
                 <div><div className="Downloadbtn">
-                {params.category=="EligibleStu"?<button type="button" class="btn btn-primary mx-2 my-2" onClick={handleDownload} disabled={DriveStu.length===0?"true":""}>Download Eligible Student data</button>:""}
+                    {params.category == "EligibleStu" ? <button type="button" class="btn btn-primary mx-2 my-2" onClick={handleDownload} disabled={DriveStu.length === 0 ? "true" : ""}>Download Eligible Student data</button> : ""}
                 </div></div>
             </div>
         </>
@@ -71,14 +76,14 @@ const DriveRecord_cnt = (props) => {
 
 
 const DriveRecord = () => {
-    const { fetchCurDrive, loading, setloading,setDriveStu,fetchDriveStu} = useContext(TpoAdminContext);
+    const { fetchCurDrive, loading, setloading, setDriveStu, fetchDriveStu } = useContext(TpoAdminContext);
     const params = useParams();
     useEffect(() => {
         console.log(params.category);
         setDriveStu([]);
         setloading(true);
         fetchCurDrive(params.id);
-        fetchDriveStu(params.category,params.id);
+        fetchDriveStu(params.category, params.id);
         setloading(false);
     }, []);
 
@@ -86,7 +91,7 @@ const DriveRecord = () => {
         <>
             <Header />
             <Navbar />
-            {loading? <Spinner /> : <DriveRecord_cnt />}
+            {loading ? <Spinner /> : <DriveRecord_cnt />}
         </>
     )
 }
